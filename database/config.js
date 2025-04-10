464d5e82894d19ae73654908a85f2edfa090be37
Jhonatan Rojas <jhonatanrojasmd@gmail.com>
2025-04-09 21:09:37 -0400

    database

--- database/config.js
+++ database/config.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        //    useNewUrlParser: true,
+          //  useUnifiedTopology: true,
     
         });
         console.log('DB online');
