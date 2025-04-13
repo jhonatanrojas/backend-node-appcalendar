2050a1f9fff5f7f98226523202b5476d90351aef
Jhonatan Rojas <jhonatanrojasmd@gmail.com>
2025-04-13 00:17:32 -0400

    se agrega front

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
 const app = express();
-
+const patch = require('path');
 //Base de datos
 
 dbConnection();
@@ -23,7 +23,7 @@ app.use(express.json());
 //rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
-
+app.use('*', (req, res) => res.sendFile(patch.resolve(__dirname, 'public/index.html')));
 
 app.listen(port, () => {
     console.log(` app listening on port ${port}`);
